9e3117f061cc092351788d55904a554df3f25c6a
neongenesiseva <rxl418@case.edu>
2017-07-24 16:37:23 -0400

    config

---
+++ node-todo-api/server/config/config.js
@@ -0,0 +1,9 @@
+var env = process.env.NODE_ENV || 'development';//just set for heroku
+
+if (env === 'development'){
+    process.env.PORT = 3300;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
+} else if (env === 'test'){
+    process.env.PORT = 3300;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
+}
