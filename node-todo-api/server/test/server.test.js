6cfe5b0ac712a15292f671ba46f5224f7997fce9
neongenesiseva <rxl418@case.edu>
2017-07-24 11:57:01 -0400

    add GET tests

--- node-todo-api/server/test/server.test.js
+++ node-todo-api/server/test/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID}=require('mongodb');
 
 const {app} = require('../server');
 const Todo = require('../models/todo.model');
 
 const todos = [{
+    _id: new ObjectID(),
     text:'First test todo'
 },{
+    _id: new ObjectID(),
     text:'Second test todo'
 }]
 
@@ -68,6 +71,35 @@ describe('GET /todos',()=>{
             .expect((res)=>{
                 expect(res.body.todos.length).toBe(2);
             })
+            .end(done);
+    });
+})
+
+describe('GET /todos/:id',()=>{
+    it ('get the todo doc',(done)=>{
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todo.text).toBe('First test todo');
+            })
+            .end(done);
+    });
+
+    it ('return 404 if no todo found',(done)=>{
+        var hexId = new ObjectID().toHexString();
+        //pass a valid hexstring id 
+        request(app)
+            .get(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('return 404 if the id is invalid',(done)=>{
+        //pass a invalid id
+        request(app)
+            .get(`/todos/123`)
+            .expect(404)
             .end(done);
     })
 })
