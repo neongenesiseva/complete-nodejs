e4d73668abcf21eafefa88a21ba380169db4ce73
neongenesiseva <rxl418@case.edu>
2017-07-25 20:02:49 -0400

    test with /users/login

--- node-todo-api/server/models/user.model.js
+++ node-todo-api/server/models/user.model.js
@@ -86,6 +86,27 @@ UserSchema.statics.findByToken = function(token){
     });
 };
 
+UserSchema.statics.findByCredentials = function(email,password){
+    var User = this;
+    
+    //why ther is a return here
+    return User.findOne({email}).then((user)=>{
+        if (!user){
+            return Promise.reject('no such user');//trigger catch(err) directly
+        } else {
+            return new Promise((resolve,reject)=>{
+            bcrypt.compare(password,user.password,(err,res)=>{
+                if (res){
+                    resolve(user);
+                } else {
+                    reject('wrong password');
+                }
+            })
+            })
+        }
+        })
+    };
+
 UserSchema.pre('save',function(next){
     var user = this;
     
